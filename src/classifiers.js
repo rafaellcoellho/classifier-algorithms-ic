cfca12815470df50ce641c1bcacdad9c1650dce2
Rafael Coelho <rafaellcoellho@gmail.com>
2018-11-07 14:45:02 -0300

    Refatora um pouco o código

---
+++ src/classifiers.js
@@ -0,0 +1,71 @@
+function euclidianDistance (dataPoint1, dataPoint2) {
+  return Math.sqrt(
+    Math.pow(dataPoint2[0] - dataPoint1[0], 2) +
+        Math.pow(dataPoint2[1] - dataPoint1[1], 2) +
+        Math.pow(dataPoint2[2] - dataPoint1[2], 2) +
+        Math.pow(dataPoint2[3] - dataPoint1[3], 2)
+  )
+}
+
+module.exports = {
+  nearestNeighborAlgorithm: (dataset, dataPoint) => {
+    const dist = dataset
+      .map(data => [ euclidianDistance(dataPoint, data), data[4] ])
+      .sort()
+
+    return dist[0][1]
+  },
+  kNearestNeighborAlgorithm: (dataset, dataPoint, k) => {
+    const dist = dataset
+      .map(data => [ euclidianDistance(dataPoint, data), data[4] ])
+      .sort()
+
+    const kNeighbor = dist
+      .slice(0, k)
+
+    let speciesOfNeighbor = { 'setosa': 0, 'versicolor': 0, 'virginica': 0 }
+    kNeighbor.forEach(neighbor => {
+      speciesOfNeighbor[neighbor[1]] += 1
+    })
+
+    const label = Object.keys(speciesOfNeighbor)
+      .reduce((a, b) => speciesOfNeighbor[a] > speciesOfNeighbor[b] ? a : b)
+
+    return label
+  },
+  dmc: (dataset, dataPoint) => {
+    const getAverage = (total, current, index, data) => {
+      // Faz o somatório total
+      total = current
+        .map((x, i) => x + total[i])
+
+      if (index === data.length - 1) {
+        // Caso seja o ultimo, retorna a média
+        return [
+          total[0] / data.length,
+          total[1] / data.length,
+          total[2] / data.length,
+          total[3] / data.length,
+          current[4]]
+      } else {
+        return total
+      }
+    }
+
+    const averageSetosa = dataset
+      .filter(data => data[4] === 'setosa')
+      .reduce(getAverage)
+
+    const averageVersicolor = dataset
+      .filter(data => data[4] === 'versicolor')
+      .reduce(getAverage)
+
+    const averageVirginica = dataset
+      .filter(data => data[4] === 'virginica')
+      .reduce(getAverage)
+
+    const averageArray = [averageSetosa, averageVersicolor, averageVirginica]
+
+    return module.exports.nearestNeighborAlgorithm(averageArray, dataPoint)
+  }
+}
